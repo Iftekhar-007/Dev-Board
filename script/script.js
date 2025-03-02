411af0395c8cdeac32f7e7614be588f6a8d14d78
Iftekhar-007 <shawonsp534@gmail.com>
2025-03-02 15:44:10 +0600

    activity logl most done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,31 @@
 let numOfRemainWork = document.getElementsByClassName("assign-card").length;
 console.log(numOfRemainWork);
 document.getElementById("remain-work").innerText = numOfRemainWork;
+let title = document.querySelectorAll(".titles");
 // let convertedRemainWork = Number(remainWork);
-let titles = document.getElementsByClassName("titles").innerText;
+
+let date = new Date();
+
+let formatDate =
+  date.getFullYear() +
+  "/" +
+  (date.getMonth() + 1) +
+  "/" +
+  date.getDate() +
+  " " +
+  "at" +
+  " " +
+  date.getHours() +
+  " " +
+  "hours" +
+  ":" +
+  date.getMinutes() +
+  " " +
+  "minutes" +
+  ":" +
+  date.getSeconds() +
+  " " +
+  "seconds";
 
 let message = document.getElementById("message");
 
@@ -23,7 +46,15 @@ document.querySelectorAll("#complete-btn").forEach((btn) => {
     document.getElementById("done-number").innerText = doneNumber;
 
     let doneMessage = document.createElement("p");
-    doneMessage.innerText = `you have complete the task ${titles} at today`;
+    doneMessage.innerText = `You have done the task: ${title.innerText} on ${formatDate}`;
+    let message = document.getElementById("message");
     message.appendChild(doneMessage);
+
+    doneMessage.style.backgroundColor = "#F4F7FF";
+    doneMessage.style.padding = "10px";
+    doneMessage.style.marginTop = "20px";
+    doneMessage.style.borderRadius = "10px";
+    doneMessage.style.fontFamily = "poppins";
+    doneMessage.style.fontSize = "1rem";
   });
 });
