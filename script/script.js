da5b4682fb01d0d2f7fe4da15825a9df45dda34d
Iftekhar-007 <shawonsp534@gmail.com>
2025-03-02 12:20:02 +0600

    latest measssage

--- script/script.js
+++ script/script.js
@@ -1,18 +1,22 @@
-let numberOfRemainWork = document.getElementsByClassName("assign-card").length;
-console.log(numberRemainWork);
-
-let remainWork = document.getElementById("remain-work"),
-  innerText;
-let convertedRemainWork = Number(remainWork);
+let numOfRemainWork = document.getElementsByClassName("assign-card").length;
+console.log(numOfRemainWork);
+document.getElementById("remain-work").innerText = numOfRemainWork;
+// let convertedRemainWork = Number(remainWork);
 
 document.querySelectorAll("#complete-btn").forEach((btn) => {
   btn.addEventListener("click", function (event) {
     event.preventDefault();
-    alert("Board Updated");
+    alert("Board Updated Successfully");
 
-    // let remainWork = document.getElementById("remain-work").innerText;
-    // let convertedRemainWork = Number(remainWork);
-    // console.log(convertedRemainWork);
-    // convertedRemainWork.innerText = remainWork - 1;
+    btn.disabled = true;
+
+    if (numOfRemainWork > 0) {
+      numOfRemainWork--;
+      document.getElementById("remain-work").innerText = numOfRemainWork;
+    }
+
+    let doneNumber = document.getElementById("done-number").innerText;
+    doneNumber = Number(doneNumber) + 1;
+    document.getElementById("done-number").innerText = doneNumber;
   });
 });
