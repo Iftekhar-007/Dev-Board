0ad9fbba27cfcb392d6b83e06bca960c1677735a
Iftekhar-007 <shawonsp534@gmail.com>
2025-03-02 11:13:45 +0600

    another added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,18 @@
-document
-  .getElementsByClassName("completed-btn")
-  .addEventListener("click", function (event) {
+let numberOfRemainWork = document.getElementsByClassName("assign-card").length;
+console.log(numberRemainWork);
+
+let remainWork = document.getElementById("remain-work"),
+  innerText;
+let convertedRemainWork = Number(remainWork);
+
+document.querySelectorAll("#complete-btn").forEach((btn) => {
+  btn.addEventListener("click", function (event) {
     event.preventDefault();
-    alert("Board Updated Successfully");
+    alert("Board Updated");
+
+    // let remainWork = document.getElementById("remain-work").innerText;
+    // let convertedRemainWork = Number(remainWork);
+    // console.log(convertedRemainWork);
+    // convertedRemainWork.innerText = remainWork - 1;
   });
+});
